54135b4ada459781f3e7cec646a9561adc06b4fc
nathkagnh <nathkagnh@gmail.com>
2018-02-27 15:19:17 +0700

    Update

--- module/Admin/src/Controller/ProductController.php
+++ module/Admin/src/Controller/ProductController.php
@@ -3,6 +3,9 @@ namespace Admin\Controller;
 
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
+use Zend\Form\Form;
+use Zend\InputFilter\InputFilter;
+use Zend\InputFilter\FileInput;
 
 class ProductController extends AbstractActionController
 {
@@ -14,10 +17,31 @@ class ProductController extends AbstractActionController
 
     public function addAction()
     {
+        //check is post
+        if($this->getRequest()->isPost())
+        {
+            $request = $this->getRequest();
+            $data = array_merge_recursive(
+                $request->getPost()->toArray(),
+                $request->getFiles()->toArray()
+            );
+
+            //save images
+            $surfix_date = date('Y/m/d');
+            $location = 'public/i/products/'.$surfix_date;
+            $allowedExtension = array('image/jpeg', 'image/png', 'image/gif');
+            $image_link = $this->saveFile($data['image'], $location, $allowedExtension);
+            $other_images_link = '';
+            if(!empty($data['other_images']))
+            {
+                foreach($data['other_images'] as $file)
+                {
+                    $other_images_link[] = $this->saveFile($file, $location, $allowedExtension);
+                }
+            }
+        }
+
         $this->layout()->setVariable('nav', 'product/add');
-        // $headScript = $this->getServiceLocator()->get('viewhelpermanager')->get('headScript');
-        // $headScript->appendFile('/ad-st/js/scripts/product.js');
-        
         return new ViewModel();
     }
 
@@ -25,5 +49,18 @@ class ProductController extends AbstractActionController
     {
         $this->layout()->setVariable('nav', 'product/edit');
         return new ViewModel();
+    }
+
+    function saveFile($file, $location, $allowedExtension)
+    {
+        $filePath = '';
+        if(!empty($file) && in_array($file['type'], $allowedExtension))
+        {
+            $fileName = $file['name'];
+            if(!file_exists($location)) mkdir($location);
+            move_uploaded_file($file['tmp_name'], $location.'/'.$fileName);
+            $filePath = '/i/products/'.$surfix_date.$fileName;
+        }
+        return $filePath;
     }
 }
--- public/ad-st/js/scripts/product.js
+++ public/ad-st/js/scripts/product.js
@@ -20,17 +20,21 @@
     $('#other_images3').dropify();
     $('#btn_more_image').click(function(){
       var current_img = $('input[name="other_images[]"]').length + 1;
-      var tmp_html = '<div class="col-xl-4 col-md-6">'+
-                        '<div class="example-wrap">'+
-                          '<h4 class="example-title">Ảnh chi tiết '+current_img+'</h4>'+
-                          '<div class="example">'+
-                            '<input type="file" id="other_images'+current_img+'" name="other_images[]"/>'+
-                          '</div>'+
-                        '</div>'+
-                      '</div>';
+      var key = (new Date).getTime();
+      var tmp_html = '<div class="col-xl-4 col-md-6">'+'<div class="example-wrap">'+'<h4 class="example-title title_other_image">Ảnh chi tiết '+current_img+'</h4>'+'<div class="example">'+'<input type="file" id="other_images'+key+'" name="other_images[]"/>'+'</div>'+'</div>'+'</div>';
       $(tmp_html).insertBefore('.col_btn_more_image');
-      $('#other_images'+current_img).dropify();
+      var input_img = $('#other_images'+key);
+      var dr = input_img.dropify();
+      dr.on('dropify.afterClear', function(event, element){
+        dr.data('dropify').destroy();
+        input_img.parent().parent().parent().remove();
+        $('.title_other_image').each(function(k, v){
+          $(this).text('Ảnh chi tiết '+(k+1));
+        });
+      });
     });
-
+    for(var i=1; i<=3; i++){
+      $('#btn_more_image').trigger('click');
+    }
   });
 });
